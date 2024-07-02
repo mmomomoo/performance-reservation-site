914ab3ce63fe62a86368283d17e6702ed9bf6ff6
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-07-02 17:55:12 +0900

    add: 회원가입 구현 완료

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { SignUpAuthDto } from './dto/sign-up-auth.dto';
+import { SignInAuthDto } from './dto/sign-in-auth.dto';
+import bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { UserRole } from 'src/users/entities/user-role.enum';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ) {}
+  //회원가입
+  async signUp(signUpAuthDto: SignUpAuthDto): Promise<void> {
+    const { email, password, userName, role } = signUpAuthDto;
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+    const existingUser = await this.findByEmail(email);
+    if (existingUser) {
+      throw new BadRequestException('이미 존재하는 이메일 입니다.');
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+    const user = this.usersRepository.create({
+      email,
+      password: hashedPassword,
+      username: userName, // username 속성으로 맞춤
+      role: role as UserRole,
+    });
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    await this.usersRepository.save(user);
+  }
+  // //로그인
+  // signIn(signInAuthDto: SignInAuthDto): Promise<{ accessToken: string }> {
+  //   return { accessToken };
+  // }
+  // signOut(id: number) {
+  //   return `This action returns a #${id} auth`;
+  // }
+
+  // token(id: number) {
+  //   return `This action returns a #${id} auth`;
+  // }
+  // 이미 존재하는 이메일인가 찾기용
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { email } });
   }
 }
---
+++ util/match.decorator.ts
@@ -0,0 +1,32 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'match', async: false })
+export class MatchConstraint implements ValidatorConstraintInterface {
+  validate(value: any, args: ValidationArguments) {
+    const [relatedPropertyName] = args.constraints;
+    const relatedValue = (args.object as any)[relatedPropertyName];
+    return value === relatedValue;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return '비밀번호가 일치하지 않습니다.';
+  }
+}
+
+export function Match(property: string, validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: MatchConstraint,
+    });
+  };
+}
