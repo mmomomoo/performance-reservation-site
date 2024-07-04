a214f50a8a55585637568bc54596a03e07820211
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-07-05 02:34:14 +0900

    add: 프로필 보기 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,38 +6,45 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRole } from './entities/user-role.enum';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('users')
 export class UsersController {
   //private > 클래스 안에서만 사용하게 하기
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  //프로필 보기 구현
+  @Get('/me')
+  @Roles(UserRole.ADMIN, UserRole.USER)
+  async readMe(@Req() req: any) {
+    const userPayload = req.user;
+    const user = await this.usersService.readMe(userPayload);
+    return user;
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.usersService.create(createUserDto);
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>
+    // private jwtService: JwtService // JWT 토큰 생성을 위해 주입한 서비스
+  ) {}
+
+  async readMe(userPayload: any) {
+    const uerId = userPayload.sub;
+    return await this.userRepository.findOne({ where: { id: uerId } });
+  }
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
     return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
