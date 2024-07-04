4fd97f0d56473ad6152e8593f1b87a248a8ed50b
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-07-04 18:10:13 +0900

    add: 공연 상세 조회

--- src/performances/performances.service.ts
+++ src/performances/performances.service.ts
@@ -21,21 +21,34 @@ export class PerformancesService {
   //공연 글 이미지 등록
   async imageCreat() {
     return await this.performanceRepository.find({
-      select: ['id', 'name'],
+      // select: ['id', 'name'],
     });
   }
   //공연 전체 조회
   async findAll() {
     return await this.performanceRepository.find({
-      select: ['id', 'name'],
+      // select: ['id', 'name'],
     });
   }
   //공연 상세 조회
-  findOne(id: number) {
-    return `This action returns a #${id} performance`;
+  async findOne(id: number) {
+    const performance = await this.performanceRepository.findOne({
+      where: { id },
+      // select: [
+      //   'id',
+      //   'name',
+      //   'description',
+      //   'category',
+      //   'location',
+      //   'price',
+      //   'dates',
+      //   'times',
+      // ],
+    });
+    return performance;
   }
   //이름으로 조회
-  searchName() {
+  async searchName() {
     return `This action returns all performances`;
   }
   //카테고리로 조회
